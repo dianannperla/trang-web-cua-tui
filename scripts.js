8c25eef4516a30e4e4175d0791a670f4feae3468
dianannperla <waafirmelyza@hotmail.com>
2025-10-05 18:44:04 +0100

    Fix typo in console log message for clarity.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('Trang Web Của Tui đã được tải thành công!');
+    console.log('Trang Web Của Tôi đã được tải thành công!');
 });
